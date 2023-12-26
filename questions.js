ee43b2030f41b2b15f294b1c0e65fb81fb7dc893
priyankaa1610 <98399528+priyankaa1610@users.noreply.github.com>
2023-12-26 18:36:35 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,255 @@
+let questions = [
+    {
+        numb:1,
+        question:" What is the capital city of Australia?",
+        answer:"C. Canberra",
+        options:[
+           "A. Melbourne ",
+           "B. Sydney",
+           "C. Canberra",
+           "D. Adelaide",
+        ]
+    },
+    {
+        numb:2,
+        question:" Who painted the “Starry Night”?",
+        answer:"A. Vincent van Gogh",
+        options:[
+            "A. Vincent van Gogh",
+            "B. Pablo Picasso",
+            "C. Claude Monet",
+            "D. Rembrandt",
+
+        ]
+    },
+    {
+        numb:3,
+        question:" In geometry, how many sides does a dodecagon have?",
+        answer:"B. 12",
+        options:[
+             
+            "A. 10",
+            "B. 12",
+            "C. 14",
+            "D. 16",
+
+        ]
+    },
+    {
+        numb:4,
+        question:" What does the “HTTP” stand for in a website address?",
+        answer:"D. Hyper Text Transfer Protocol",
+        options:[
+            "A. High Technology Transfer Protocol",
+            "B. High Transfer Text Protocol",
+            "C. Hyper Text Technology Protocol",
+            "D. Hyper Text Transfer Protocol",
+
+        ]
+    },
+    {
+        numb:5,
+        question:" Which chemical element has the symbol Au?",
+        answer:"C. Gold",
+        options:[
+            "A. Silver",
+            "B. Aluminium",
+            "C. Gold",
+            "D. Argon",
+
+
+        ]
+    },
+    {
+        numb:6,
+        question:" What is the square root of 144?",
+        answer:"A. 12",
+        options:[
+            "A. 12",
+            "B. 14",
+            "C. 16",
+            "D. 18",
+        ]
+    },
+    {
+        numb:7,
+        question:" What is the most abundant gas in Earth’s atmosphere?",
+        answer:"B. Nitrogen",
+        options:[
+            "A. Oxygen",
+            "B. Nitrogen",
+            "C. Carbon dioxide",
+            "D. Argon",
+
+
+        ]
+    },
+    {
+        numb:8,
+        question:" What is the name of a polygon with eight sides?",
+        answer:"C. Octagon",
+        options:[
+            "A. Hexagon",
+            "B. Heptagon",
+            "C. Octagon",
+            "D. Nonagon",
+
+
+        ]
+    },
+    {
+        numb:9,
+        question:" Who is the founder of Tesla?",
+        answer:"C. Elon Musk",
+        options:[
+            "A. Bill Gates",
+            "B. Jeff Bezos",
+            "C. Elon Musk",
+            "D. Mark Zukenburg",
+
+
+        ]
+    },
+    {
+        numb:10,
+        question:" What force is responsible for keeping us on the ground?",
+        answer:"D. Gravitational",
+        options:[
+            "A. Magnetic",
+            "B. Centrifugal",
+            "C. Frictional",
+            "D. Gravitational",
+
+
+        ]
+    },
+    {
+        numb:11,
+        question:" Which musical instrument has keys, pedals, and strings?",
+        answer:"B. Piano",
+        options:[
+            "A. Violin",
+            "B. Piano",
+            "C. Guitar",
+            "D. Flute",
+   ]
+    },
+    {
+        numb:12,
+        question:" Which planet in our solar system is known for its rings?",
+        answer:"C. Saturn",
+        options:[
+            "A. Venus",
+            "B. Mars",
+            "C. Saturn",
+            "D. Neptune",
+
+
+        ]
+    },
+    {
+        numb:13,
+        question:" Which country has won the most FIFA World Cup tournaments?",
+        answer:"D. Brazil",
+        options:[
+            "A. Spain",
+            "B. Germany",
+            "C. Argentina",
+            "D. Brazil",
+
+
+        ]
+    },
+    {
+        numb:14,
+        question:" What is the value of pi to three decimal places?",
+        answer:"A. 3.141",
+        options:[
+            "A. 3.141",
+            "B. 3.142",
+            "C. 3.143",
+            "D. 3.144",
+
+
+        ]
+    },
+    {
+        numb:15,
+        question:" What is the hardest known natural material on Earth?",
+        answer:"C. Diamond",
+        options:[
+            "A. Iron",
+            "B. Graphite",
+            "C. Diamond",
+            "D. Grnaite",
+
+
+        ]
+    },
+    {
+        numb:16,
+        question:" Mount Everest is located in which mountain range?",
+        answer:"D. Himalyas",
+        options:[
+            "A. Andes",
+            "B. Alps",
+            "C. Rocky",
+            "D. Himalyas",
+
+
+        ]
+    },
+    {
+        numb:17,
+        question:" Who played the character of Harry Potter in the movie series?",
+        answer:"B. Daniel Radcliffe",
+        options:[
+            "A. Rupert Grint",
+            "B. Daniel Radcliffe",
+            "C. Tom Felton",
+            "D. Matthew Lewis",
+
+
+        ]
+    },
+    {
+        numb:18,
+        question:" The “Mona Lisa” is displayed in which museum?",
+        answer:"B. The Louvre",
+        options:[
+            "A. The Metropolitan Museum of Art",
+            "B. The Louvre",
+            "C. The Uffizi Gallery",
+            "D. The Prado Museum",
+
+
+        ]
+    },
+    {
+        numb:19,
+        question:" Who is the highest run-scorer in the history of Test cricket?",
+        answer:"A. Sachin Tendulkar",
+        options:[
+            "A. Sachin Tendulkar",
+            "B. Brian Lara",
+            "C. Ricky Ponting",
+            "D. Jacques Kallis",
+
+
+        ]
+    },
+    {
+        numb:20,
+        question:" What is the name of the first man-made satellite to orbit the Earth?",
+        answer:"C. Sputnik 1",
+        options:[
+            "A. Explorer 1",
+            "B. Voyager 1",
+            "C. Sputnik 1",
+            "D. Apollo 11",
+
+
+        ]
+    }
+]
+
